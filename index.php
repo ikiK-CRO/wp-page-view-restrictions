8eceae43761b565a4884d8d84cfd0351ff2bfbaa
ikiK <kiki.ikik@gmail.com>
2021-05-19 19:16:09 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: WP Page View Restrictions
+* Plugin URI: https://wpcompress.appdiz-informatika.hr/
+* Description: This is the very first plugin I ever created.
+* Version: 1.0.0
+* Author: Kristijan Bičak
+* Author URI: www.appdiz-informatika.hr
+**/
